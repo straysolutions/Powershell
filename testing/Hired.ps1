4b519cba0bad5fc3005325c254bfb68afcfb2095
imveryhighrightnow <59445285+imveryhighrightnow@users.noreply.github.com>
2020-01-02 09:23:29 -0600

    Add files via upload

---
+++ testing/Hired.ps1
@@ -0,0 +1,94 @@
+﻿#prompt for info and create account based on location
+
+$firstname = Read-Host -Prompt 'First Name?'
+$lastname = Read-Host -Prompt 'Last Name?'
+$adname = Read-Host -Prompt 'Username?'
+$title = Read-Host -Prompt 'Title?'
+$password = Read-Host "Enter password" -AsSecureString
+
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Department???'
+     )
+     cls
+     Write-Host "================ $Title ================"
+     
+     Write-Host "1: Press '1' for Brazos."
+     Write-Host "2: Press '2' for DEV."
+     Write-Host "3: Press '3' for DFW."
+     Write-Host "4: Press '4' for Email Only."
+     Write-Host "5: Press '5' for Houston."
+     Write-Host "6: Press '6' for MCIP."
+     Write-Host "7: Press '7' for Med Call."
+     Write-Host "8: Press '8' to Outside Sales."
+     Write-Host "9: Press '9' to Plano."
+     Write-Host "10: Press '10' to SS."
+     Write-Host "11: Press '11' to Tyler."
+}
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                cls
+                'You chose option #1'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Brazos,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+                
+           } '2' {
+                cls
+                'You chose option #2'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=DEV,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '3' {
+                cls
+                'You chose option #3'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=DFW,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '4' {
+                cls
+                'You chose option #4'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Email Only,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '5' {
+                cls
+                'You chose option #5'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Houston,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '6' {
+                cls
+                'You chose option #6'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=MCIP,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '7' {
+                cls
+                'You chose option #7'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=MedCall,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '8' {
+                cls
+                'You chose option #8'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Outside Sales,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '9' {
+                cls
+                'You chose option #9'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Plano,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title"  -EmailAddress "$adname@mchest.com"                 
+           } '10' {
+                cls
+                'You chose option #10'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=SS,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -Title "$title" -EmailAddress "$adname@mchest.com"
+           } '11' {
+                cls
+                'You chose option #11'
+                New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$adname" -DisplayName "$firstname $lastname" -Description "$title" -UserPrincipalName "$adname@mchest.net" -Path "OU=Tyler,OU=Location,DC=mchest,DC=net" -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $true -ScriptPath "login.bat" -Title "$title" -EmailAddress "$adname@mchest.com"                  
+                 
+                 
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input)
+
+#AD User mirror groupmembership from other user
+
+$CopyFromUser = Get-ADUser (Read-Host -Prompt 'Input the user name to copy group membership from') -prop MemberOf
+$CopyToUser = $adname
+$CopyFromUser.MemberOf | Where{$CopyToUser.MemberOf -notcontains $_} |  Add-ADGroupMember -Members $CopyToUser
---
+++ testing/hremployeeaudit.ps1
@@ -0,0 +1,56 @@
+﻿
+#remove spaces from headers so they can be called properly
+$a = Get-Content "C:\temp\Monthly Employee Update.csv"
+
+
+$a[0] = $a[0] -replace " ", ""
+
+$a | Set-Content "C:\temp\Monthly Employee Update.csv"
+
+#combines manager names
+Import-Csv "C:\temp\Monthly Employee Update.csv" | Select-Object @{Name = "Manager"; Expression = {$_."ReportsToFirstName" + " " + $_."ReportsToLastName"}}, * | Export-Csv -NoTypeInfo -Path "C:\temp\withmanagername.csv"
+
+#replaces some proper manager names with how they are listed in AD
+[io.file]::readalltext(“C:\temp\withmanagername.csv”).replace(“Douglas Sloan”,”Doug Sloan”) | Out-File “C:\temp\withmanagername.csv” -Encoding ascii –Force
+[io.file]::readalltext(“C:\temp\withmanagername.csv”).replace(“Jason Haire”,”Jason P. Haire”) | Out-File “C:\temp\withmanagername.csv” -Encoding ascii –Force
+[io.file]::readalltext(“C:\temp\withmanagername.csv”).replace(“William Mayeux”,”William H. Mayeux”) | Out-File “C:\temp\withmanagername.csv” -Encoding ascii –Force
+[io.file]::readalltext(“C:\temp\withmanagername.csv”).replace(“Joshua Kennedy”,”Josh Kennedy”) | Out-File “C:\temp\withmanagername.csv” -Encoding ascii –Force
+[io.file]::readalltext(“C:\temp\withmanagername.csv”).replace(“Daniel Rich”,”Danny Rich”) | Out-File “C:\temp\withmanagername.csv” -Encoding ascii –Force
+
+#removes spaces from the end of the job title field
+Import-Csv "C:\temp\withmanagername.csv" | ForEach-Object {
+        $_.JobTitleDescription = $_.JobTitleDescription.trim()
+        $_
+} | Export-Csv "C:\temp\clean.csv" -NoTypeInformation
+
+
+# Import CSV from HR
+$HR = import-csv -path "C:\temp\clean.csv"
+# pull all users in the OU that have a title and email
+$adUsers = get-aduser -filter {title -like "*" -and mail -like "*"  } -SearchBase "OU=Location,DC=MCHEST,DC=NET" -Properties givenname, surname, title, manager | Select-Object givenname, surname, title,@{name='ManagerName';expression={(Get-ADUser -Identity $_.manager | Select-Object -ExpandProperty name)}} | sort-object givenname
+# array of users that match our known good input
+$matchUsers = @()
+# array of users NOT matching our known good input
+$notMatchUsers = @()
+
+foreach ($user in $adUsers){
+
+    #makes sure title and manager match. Left off name as we do not force AD to have proper name
+    if ($HR.JobTitleDescription -eq $user.title -and $HR.manager -eq $user.ManagerName) {
+        $matchUsers += $user
+    } 
+    else {
+
+    $notMatchUsers += $user
+    }
+
+
+}
+
+#creates two files to show matched and not matched.
+$matchUsers | export-csv  -NoTypeInformation -Path "c:\temp\match.csv"
+$notmatchusers | export-csv   -NoTypeInformation -path "c:\temp\notmatch.csv"
+
+#little bit of clean up. Leaves original CSV so you can compare to notmatch csv
+Remove-Item -Path "C:\temp\withmanagername.csv" -Force
+Remove-Item -Path "C:\temp\clean.csv" -Force
